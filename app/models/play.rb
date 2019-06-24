212f706f7bb8bdf1a29358dc3411de3fadea1965
Mary Kate <MaryKate@Marys-MacBook-Pro-3.local>
2019-06-24 18:16:58 -0400

    render images with link on index

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,6 @@
 class Play < ApplicationRecord
     has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+    validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 
     belongs_to :user
     belongs_to :category
